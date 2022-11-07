c9d7ac651db3a433c78f3383638e4270713eb253
Jonathan Bridge <jvbridge@gmail.com>
2022-11-07 09:03:22 -0800

    boilerplate components for header and fooder

---
+++ src/components/footer/main.jsx
@@ -0,0 +1,7 @@
+import { Navbar } from "react-bootstrap";
+
+function Footer(){
+  return <Navbar>Footer</Navbar>
+}
+
+export default Footer
---
+++ src/components/header/main.jsx
@@ -0,0 +1,7 @@
+import { Navbar } from "react-bootstrap";
+
+function Header(){
+  return <Navbar>Header</Navbar>
+}
+
+export default Header
