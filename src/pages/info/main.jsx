d7340cfe217f70b6ad7b140d3418b882bf5e37ff
Jonathan Bridge <jvbridge@gmail.com>
2022-11-08 10:06:59 -0800

    stubs for starter pages

---
+++ src/pages/info/main.jsx
@@ -0,0 +1,8 @@
+function info (){
+  return <p>
+    This is a game balance simulator. It is used in order to simulate resources
+    gained/lost over periods of time in order 
+  </p>
+}
+
+export default info;
