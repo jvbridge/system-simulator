4f2cc0d3124b55af205e34836b70487422cb6ee5
Jonathan Bridge <jvbridge@gmail.com>
2022-11-08 11:10:15 -0800

    made the component pull the text appropriately

--- src/pages/info/index.jsx
+++ src/pages/info/index.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import markdownPath from './info.md'
 
-function InfoPage (){
+ function InfoPage (){
+
+  const [markdownText, setMarkdownText] = useState("Loading...");
+
+  const fetchInfo = async () =>{
+    try{
+      const res = await fetch(markdownPath);
+      const mdText = await res.text();
+      setMarkdownText(mdText);
+    } catch(error){
+      console.error("Got error fetching: ");
+      console.error(error);
+    }
+  }
+
+  fetchInfo();
+
   return ( 
-  <>
-  {/* <ReactMarkdown>
-    #hello World
-  </ReactMarkdown> */}
-  <p>
-    This is the info tag
-  </p>
-  </>
+  <ReactMarkdown children={markdownText}/>
   )
 }
 
